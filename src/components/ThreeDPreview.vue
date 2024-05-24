c53a71afd7537a0058a153bdb3c0d8eecec440b8
Sven Hemmer <sven.hemmer@hiqs.de>
2024-05-24 23:30:18 +0200

    Move three specific code away from vue component

--- src/components/ThreeDPreview.vue
+++ src/components/ThreeDPreview.vue
@@ -6,23 +6,14 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
+import { renderer, scene, camera } from '../utils/threeDUtils';
 import type { Ref } from 'vue';
-import * as THREE from 'three';
 
 export default defineComponent({
     name: 'ThreeDPreview',
     setup() {
         const threeContainer: Ref<HTMLDivElement | undefined> = ref();
 
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-        const renderer = new THREE.WebGLRenderer();
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
-
-        camera.position.z = 5;
 
         const initThree = () => {
             renderer.setSize(threeContainer.value!.offsetWidth, threeContainer.value!.offsetHeight);
@@ -59,4 +50,4 @@ export default defineComponent({
     align-items: center;
     justify-content: center;
 }
-</style>
+</style>../utils/threeDUtils
