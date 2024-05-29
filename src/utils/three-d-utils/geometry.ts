59285a480607ffc3393705f6ab9dddf2e50fac8e
Sven Hemmer <sven.hemmer@hiqs.de>
2024-05-30 01:58:07 +0200

    Add ring

--- src/utils/three-d-utils/geometry.ts
+++ src/utils/three-d-utils/geometry.ts
@@ -169,12 +169,37 @@ const planeGeometry: GeometryWrapper = {
     }       
 }
 
+const ringFields = [
+    { name: 'Dimensions', sub: [
+        { name: 'Radius inner', field: createField('radiusInner', 1) },
+        { name: 'Radius outer', field: createField('radiusOuter', 1) }
+    ]},
+    { name: 'Segments', sub: [
+        { name: 'Theta', field: createField('theta', 20) },
+        { name: 'Phi', field: createField('phi', 5) },
+        { name: 'Open ended', field: createField('openEnded', false) },
+    ]},
+    { name: 'Angle', sub: [
+        { name: 'Start', field: createField('start', 0) },
+        { name: 'Length', field: createField('length', 2 * Math.PI) }
+    ]}
+];
+
+
 const ringGeometry: GeometryWrapper = {
     name: 'Ring',
+    fields: ringFields,
     position: { x: 1, y: 1, z: 1 },
     rotation: { x: 0, y: 0, z: 0 },
     create: () => {
-        const geom = new RingGeometry(ringGeometry.position.x, ringGeometry.position.y, ringGeometry.position.z );
+        const geom = new RingGeometry(
+            ringGeometry.fields![0].sub![0].field!.getValue() as number,
+            ringGeometry.fields![0].sub![1].field!.getValue() as number,
+            ringGeometry.fields![1].sub![0].field!.getValue() as number,
+            ringGeometry.fields![1].sub![1].field!.getValue() as number,
+            ringGeometry.fields![2].sub![0].field!.getValue() as number,
+            ringGeometry.fields![2].sub![1].field!.getValue() as number,
+        );
         return geom;
     }       
 }
